07b458db7d3b5d3ed0ffa52db0545d5fc519d277
Connor Goddard <clg11@aber.ac.uk>
2013-06-06 11:13:07 +0100

    Initial commit of project

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameOfLife.Engine.Base;
+
+namespace GameOfLife
+{
+    public class Cell : CellInterface
+    {
+
+        public int rowIndex { get; set; }
+
+        public int columnIndex { get; set; }
+
+        public bool isAlive { get; set; }
+
+    }
+}
---
+++ GameOfLife/CellRuleInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameOfLife.Engine.Base;
+
+namespace GameOfLife
+{
+    public interface CellRuleInterface<TC, TG> where TC:CellInterface
+                                               where TG:GridInterface<TC>
+
+    {
+
+        void Execute(TC theCell)
+        {
+            Console.Beep();
+        }
+    }
+}
