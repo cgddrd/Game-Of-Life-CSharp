e8cc177c8df9346a5ec10fba8c6c063869fa9185
Connor Goddard <clg11@aber.ac.uk>
2013-06-23 12:52:38 +0200

    Added additional comments to explain class generic type parameters and 'where' keyword.

--- GameOfLife/Engine/Base/GridInterface.cs
+++ GameOfLife/Engine/Base/GridInterface.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace GameOfLife.Engine.Base
 {
+    /* '<ICell>' is just a name for the class parameter type. It could have been called anything.
+     * Therefore, the class is basically using a generic type, but by using the 'where' keyword, 
+     * it specifies that any types used as parameters for the generic class 'GridInterface' MUST in some
+     * implement the 'CellInterface' interface. 
+     * 
+     * Basically, only classes that implement 'CellInterface' (e.g. a cell) can
+     * be used with this generic class. 
+     */
     public interface GridInterface<ICell> where ICell : CellInterface
     {
 
